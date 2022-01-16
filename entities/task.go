98f4498506d5b8c0a6ac0b43e75ed2fc536d37ac
Ilham Junius <ilhamjunius712@gmail.com>
2022-01-16 19:56:03 +0700

    update entitiy

--- entities/task.go
+++ entities/task.go
@@ -5,9 +5,9 @@ import "gorm.io/gorm"
 type Task struct {
 	gorm.Model
 	ID        uint   `json:"id" form:"id"`
-	Name      string `json:"name" form:"name" gorm:"uniqueIndex;not null"`
-	Desc      string `json:"desc" form:"desc" gorm:"not null"`
-	Status    string `json:"status" form:"status" gorm:"not null"`
-	UserID    uint   `json:"user_id" form:"user_id" gorm:"not null"`
+	Name      string `json:"name" form:"name"`
+	Desc      string `json:"desc" form:"desc"`
+	Status    string `json:"status" form:"status"`
+	UserID    uint   `json:"user_id" form:"user_id"`
 	ProjectID uint   `json:"project_id" form:"project_id" gorm:"default:NULL"`
 }
