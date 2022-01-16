7f9444dd3392279d31c04108c5bc6f42f6f728d1
Ilham Junius <ilhamjunius712@gmail.com>
2022-01-16 20:15:24 +0700

    update entitiy

--- entities/project.go
+++ entities/project.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Project struct {
 	gorm.Model
 	ID    uint   `json:"id" form:"id"`
-	Name  string `json:"name" form:"name" gorm:"uniqueIndex;" validate:"required"`
+	Name  string `json:"name" form:"name" gorm:"unique;"`
 	Tasks []Task `json:"tasks" form:"tasks"`
 }
